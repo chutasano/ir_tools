fc84eaeb71f0261187459311e9c67171bbf10573
Chuta Sano <chuta_sano@student.uml.edu>
2017-11-17 22:44:41 +0000

    Added mosquitto support on amazon ec2 (or any generic mosquitto broker)

---
+++ mqtt-sub.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# adapted from www.cs.uml.edu/~cgao
+
+import sys                                 
+import ssl
+import json
+import paho.mqtt.client as mqtt
+import subprocess as s
+
+#change these based on your setup
+muser="username"
+mpass="password"
+murl="public ip of ec2"
+mport=1234 # port number as an int
+
+#called while client tries to establish connection with the server 
+def on_connect(mqttc, obj, flags, rc):
+    if rc==0:
+        print ("Subscriber Connection status code: "+str(rc)+" | Connection status: successful")
+        mqttc.subscribe("ircode", qos=0)
+    elif rc==1:
+        print ("Subscriber Connection status code: "+str(rc)+" | Connection status: Connection refused")
+
+#called when a topic is successfully subscribed to
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos)+"data"+str(obj))
+
+#called when a message is received by a topic
+def on_message(mqttc, obj, msg):
+    print("Received message from topic: "+msg.topic+" | QoS: "+str(msg.qos)+" | Data Received: "+str(msg.payload))
+    if (msg.topic == "ircode"):
+        s.call(["./sender", msg.payload])
+        print ("sent")
+
+#creating a client with client-id=mqtt-test
+mqttc = mqtt.Client(client_id="pi")
+mqttc.username_pw_set(username=muser,password=mpass)
+
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_message = on_message
+
+#connecting to aws-account-specific-iot-endpoint
+mqttc.connect(murl, port=mport) #AWS IoT service hostname and portno
+
+#automatically handles reconnecting
+mqttc.loop_forever()
